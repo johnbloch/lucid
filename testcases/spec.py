c1926586dddb677ab5cf182897815c50d39152b7
John Bloch <john.bloch@duke.edu>
2024-06-18 07:13:14 -0400

    Fixed formatting

--- testcases/spec.py
+++ testcases/spec.py
@@ -1,6 +1,5 @@
 from ip_utils import *
 from z3 import *
-import re
 
 prog1 = "prog1"
 prog2 = "prog2"
@@ -9,5 +8,14 @@ src_ip = Int('src_ip')
 dst_ip = Int('dst_ip')
 port = Int('port')
 
-predicate = And(is_in_prefix(src_ip, 0, 0), is_in_prefix(dst_ip, 0b11111100, 6), port == 0)
-relation = lambda out1, out2: out1.port == 1 and out2.port == 0
+predicate_char_mapping = {
+    "a": And(is_in_prefix(src_ip, 0, 0), is_in_prefix(dst_ip, 0b11111100, 6), port == 0),
+    "b": And(is_in_prefix(src_ip, 0, 0), is_in_prefix(dst_ip, 0b00000000, 6), port == 1),
+    "c": And(is_in_prefix(src_ip, 0, 0), is_in_prefix(dst_ip, 0, 0), Or(port == 0, port == 1)) 
+}
+relation_char_mapping = {
+    "a": lambda out1, out2: out1.port == 1 and out2.port == 0,
+    "b": lambda out1, out2: out1.port == 0 and out2.port == 1
+}
+predicate = r'a{3}b{3}c'
+relation = r'a{3}b{4}'
--- testcases/testcases.py
+++ testcases/testcases.py
@@ -3,17 +3,16 @@ from ip_utils import *
 import json
 import subprocess
 import os
+import rstr
 
-# given 2 programs prog1.dpt and prog2.dpt, and a predicate/relation pair:
-# 1) use z3 to find input packet that satisfies predicate 
-# 2) create config files for prog1 and prog2 with same input packet
+# given 2 programs prog1.dpt and prog2.dpt, and a predicate/relation pair (where predicate is a regular expression, relation is ?)
+# 1) use z3 to find input packet stream that satisfies predicate 
+# 2) create config files for prog1 and prog2 with same input packet stream
 # 3) read outputs of prog1 and prog2 and use relation to either pass/fail test case
 
-# NEXT STEPS: 
-# 1) figure out how to use -i flag to get output events as JSON X
-# 2) use simplest possible topology X
-# 3) implement ip in prefix X
-# 3) start thinking about regular languages
+# TO DO
+# 1) Monday: implement first relation idea
+# 2) Tuesday: implement second relation idea, multiple predicate/relation pairs
 
 class IP_pkt:
     def __init__(self, src_ip, dst_ip, port): 
@@ -21,9 +20,18 @@ class IP_pkt:
         self.dst_ip = dst_ip
         self.port = port
     def __repr__(self):
-        return (f"source_ip={int_to_ip(self.src_ip)}, destination_ip={int_to_ip(self.dst_ip)}, port={self.port}")
+        return (f"(source_ip={int_to_ip(self.src_ip)}, destination_ip={int_to_ip(self.dst_ip)}, port={self.port})")
 
-# takes a predicate and generates a packet that satisfies the predicate
+# takes a predicate (as a regular expression) and returns a list of IP_packet objects that satisfies the p[predicate]
+def gen_pkt_stream(predicate, char_mapping):
+    stream = []
+    char_representation = rstr.xeger(predicate)
+    for ch in char_representation:
+        pkt = gen_pkt(char_mapping[ch])
+        stream.append(pkt)
+    return stream
+
+# takes a predicate (as a Z3 constraint) and generates a packet that satisfies the predicate
 def gen_pkt(predicate):
     src_ip = Int('src_ip')
     dst_ip = Int('dst_ip')
@@ -37,15 +45,21 @@ def gen_pkt(predicate):
     else:
         return None
 
-# takes a packet object and file name and generates a config file that sends the packet 
-def gen_config(pkt, file_name):
+# takes a list of packet objects and file name and generates a config file that sends the packet stream
+def gen_config(pkt_stream, file_name):
     config = {}
     config["max time"] = 9999999999
     event = {}
-    event["name"] = "ip_pkt"
-    event["args"] = [pkt.src_ip, pkt.dst_ip]
-    event["locations"] = [f"0:{pkt.port}"]
-    config["events"] = [event]
+    config["events"] = []
+    t = 0
+    for pkt in pkt_stream:
+        event = {}
+        event["name"] = "ip_pkt"
+        event["args"] = [pkt.src_ip, pkt.dst_ip]
+        event["locations"] = [f"0:{pkt.port}"]
+        event["timestamp"] = t
+        t += 600
+        config["events"].append(event)
     json_obj = json.dumps(config, indent=4)
     with open(file_name+".json", "w") as file:
         file.write(json_obj)
@@ -60,23 +74,31 @@ def get_lucid_dir():
 
 # parses lucid program output to create list of packet objects 
 def parse_lucid_output(output): 
-    output = json.loads(output)
-    packet = IP_pkt(int(output["args"][0]), int(output["args"][1]), int(output["locations"][0].split(":")[1]))
-    return packet
+    json_objs = [json.loads(json_obj) for json_obj in output.strip().split("\n")]
+    packet_stream = []
+    for json_obj in json_objs:
+        pkt = IP_pkt(int(json_obj["args"][0]), int(json_obj["args"][1]), int(json_obj["locations"][0].split(":")[1]))
+        packet_stream.append(pkt)
+    return packet_stream
         
-# runs Lucid program and returns output of a single switch
+# runs Lucid program and returns output of switch
 def run_prog(prog):
     path = os.path.join(os.getcwd(), prog) 
     r = subprocess.Popen(["./dpt", path, "-i"], stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             stdin=subprocess.PIPE, text=True, cwd=get_lucid_dir())
     stdout, stderr = r.communicate(timeout=10)
-    pkt = parse_lucid_output(stdout)
-    return pkt
+    pkt_stream = parse_lucid_output(stdout)
+    return pkt_stream
 
 # takes outputs of prog1 and prog2 and compares them using the relation specified by programmer
-def validate_outputs(out1, out2, relation):
-    return relation(out1, out2)
+def validate_outputs(out1, out2, relation, relation_char_mapping):
+    char_representation = rstr.xeger(relation)
+    for i in range(len(char_representation)):
+        ch = char_representation[i]
+        if not relation_char_mapping[ch](out1[i], out2[i]):
+            return False
+    return True
 
 def main():
     with open('spec.py', 'r') as file:
@@ -91,17 +113,17 @@ def main():
       if key not in local_vars:
         print(f"the spec must define {key}")
         exit(1)
-    prog1, prog2, predicate, relation = local_vars['prog1'], local_vars['prog2'], local_vars['predicate'], local_vars['relation']
+    prog1, prog2, predicate_char_mapping, relation_char_mapping, predicate, relation = local_vars['prog1'], local_vars['prog2'], local_vars['predicate_char_mapping'], local_vars['relation_char_mapping'], local_vars['predicate'], local_vars['relation']
     # 1) use z3 to find input packet that satisfies predicate
-    packet = gen_pkt(predicate)
+    packet_stream = gen_pkt_stream(predicate, predicate_char_mapping)
     # 2) create config files for prog1 and prog2 with same input packet
-    gen_config(packet, prog1)
-    gen_config(packet, prog2)
+    gen_config(packet_stream, prog1)
+    gen_config(packet_stream, prog2)
     
-    # 3) read outputs of prog1 and prog2 and use relation to either pass/fail test case
+    # # 3) read outputs of prog1 and prog2 and use relation to either pass/fail test case
     out1 = run_prog(prog1 + ".dpt")
     out2 = run_prog(prog2 + ".dpt")
-    if validate_outputs(out1, out2, relation):
+    if validate_outputs(out1, out2, relation, relation_char_mapping):
         print("Test Case Passed")
     else:
         print("Test Case Failed")
